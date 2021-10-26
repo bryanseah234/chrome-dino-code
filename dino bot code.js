61329aeae1c141b49c9f4fa95544a9ffeec61138
bryanseah234 <bryanseah234@gmail.com>
2021-10-26 16:46:32 +0800

    Update and rename dino bot code.txt to dino bot code.js

--- dino bot code.js
+++ dino bot code.js
@@ -1,12 +1,12 @@
-#to start: 
+/// to start: 
 var original = Runner.prototype.gameOver
 Runner.prototype.gameOver = function(){}
 
-#to stop:
+/// to stop:
 Runner.prototype.gameOver = original
 
-#to set speed:
+/// to set speed:
 Runner.instance_.setSpeed(1000)
 
-#reference link:
-#https://mathewsachin.github.io/blog/2016/11/05/chrome-dino-hack.html
+/// reference link:
+/// https://mathewsachin.github.io/blog/2016/11/05/chrome-dino-hack.html
